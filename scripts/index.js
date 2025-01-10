0e41214ae78af43671fb0be63a8f1605334178a0
juan_quiles <fygarimon@yahoo.com>
2025-01-10 13:06:51 -0600

    Adding javascript

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const initialCards = {
+  name: new set(),
+  link: new set(),
+};
+
+initialCards.name.push("Val Thorens");
+initialCards.link.push(
+  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
+);
+
+initialCards.name.push("Restaurant terrace");
+initialCards.link.push(
+  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
+);
+
+initialCards.name.push("An outdoor cafe");
+initialCards.link.push(
+  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
+);
+
+initialCards.name.push(
+  "A very long bridge, over the forest and through the trees"
+);
+initialCards.link.push(
+  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
+);
+
+initialCards.name.push("Tunnel with morning light");
+initialCards.link.push(
+  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
+);
+
+initialCards.name.push("Mountain house");
+initialCards.link.push(
+  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
+);
