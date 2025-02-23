362f7f640456c67f7681ff835cb15c5afbed7e61
juan_quiles <fygarimon@yahoo.com>
2025-02-22 18:14:24 -0600

    Modal on and off

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,38 @@
-const initialCards = {
-  name: new set(),
-  link: new set(),
-};
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
-initialCards.name.push("Val Thorens");
-initialCards.link.push(
-  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"
-);
+const modal = document.querySelector("#edit-modal");
+const editButtonProfile = document.querySelector(".profile__avatar-edit");
+const closeButtonModal = modal.querySelector(".modal__close");
 
-initialCards.name.push("Restaurant terrace");
-initialCards.link.push(
-  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"
-);
+editButtonProfile.addEventListener("click", function () {
+  modal.classList.add("modal_opened");
+});
 
-initialCards.name.push("An outdoor cafe");
-initialCards.link.push(
-  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"
-);
-
-initialCards.name.push(
-  "A very long bridge, over the forest and through the trees"
-);
-initialCards.link.push(
-  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"
-);
-
-initialCards.name.push("Tunnel with morning light");
-initialCards.link.push(
-  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"
-);
-
-initialCards.name.push("Mountain house");
-initialCards.link.push(
-  "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"
-);
+closeButtonModal.addEventListener("click", function () {
+  modal.classList.remove("modal_opened");
+});
