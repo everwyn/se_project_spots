57fd5b485d0ea55e818fb7214911bcf957b4b9d6
juan_quiles <fygarimon@yahoo.com>
2025-03-04 18:25:07 -0600

    Java scripting sprint 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+// Card creator for initial cards
+
+const cardTemplate = document.querySelector("#cards__template");
+const cards = document.querySelector(".cards");
+
 const initialCards = [
   {
     name: "Val Thorens",
@@ -25,14 +30,57 @@ const initialCards = [
   },
 ];
 
-const modal = document.querySelector("#edit-modal");
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".cards__card")
+    .cloneNode(true);
+
+  const cardName = cardElement.querySelector(".cards__card-description");
+  const cardLink = cardElement.querySelector(".cards__card-image");
+  const cardAlt = cardElement.querySelector(".cards__card-image");
+
+  cardName.textContent = data.name;
+  cardAlt.alt = data.name;
+  cardLink.src = data.link;
+
+  return cardElement;
+}
+
+for (let i = 0; i <= initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cards.prepend(cardElement);
+}
+
+//Modal edit for profile section
+const profileFormElement = document.querySelector("#edit-modal");
 const editButtonProfile = document.querySelector(".profile__avatar-edit");
-const closeButtonModal = modal.querySelector(".modal__close");
+const closeButtonModal = profileFormElement.querySelector(".modal__close");
+const modalFormElement = profileFormElement.querySelector(".modal__save");
 
-editButtonProfile.addEventListener("click", function () {
-  modal.classList.add("modal_opened");
-});
+const profileNameElement = document.querySelector(".profile__avatar-name");
+const profileJobElement = document.querySelector(
+  ".profile__avatar-description"
+);
 
-closeButtonModal.addEventListener("click", function () {
-  modal.classList.remove("modal_opened");
-});
+const nameInput = profileFormElement.querySelector(".modal__name");
+const jobInput = profileFormElement.querySelector(".modal__description");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let profileNameEdit = nameInput.value;
+  let profileJobEdit = jobInput.value;
+
+  profileNameElement.textContent = profileNameEdit;
+  profileJobElement.textContent = profileJobEdit;
+
+  toggleModal();
+}
+
+function toggleModal() {
+  profileFormElement.classList.toggle("modal_opened");
+}
+
+editButtonProfile.addEventListener("click", toggleModal);
+closeButtonModal.addEventListener("click", toggleModal);
+modalFormElement.addEventListener("click", handleProfileFormSubmit);
